bd99b03c82b0cff2e24f4bbe5398ba5a3c520501
5ncreation <nbastien01@gmail.com>
2015-12-28 21:19:55 +0100

    initial

---
+++ .tmp/scripts/app.js
@@ -0,0 +1,14 @@
+/*!
+ * 5N Creation Scripts
+ * Author: 5Ncreation
+ * Email: info@5ncreation.com
+ */
+
+(function($) {
+  'use strict';
+
+  $(function() {
+
+  });
+
+})(jQuery);
---
+++ .tmp/scripts/head.js
@@ -0,0 +1,8 @@
+/*!
+ * Head.JS
+ * P.S. Included all scripts, that necessary to place at the head tag, except some specific old IE polyfills
+ * Author: 5Ncreation
+ * Email: info@5ncreation.com
+ */
+
+
